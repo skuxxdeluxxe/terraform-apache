c5e1491462957483afcf605ceea1948b5871dfdd
Shantanu Padhye <shantanu.padhye@myob.com>
2021-03-21 12:16:57 +1300

    Aded terraform files

---
+++ infrastructure/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+	default = "ap-southeast-2"
+}
+
+variable "vpc_cidr" {
+	default = "10.20.0.0/16"
+}
+
+variable "subnets_cidr" {
+	type = "list"
+	default = ["10.20.1.0/24", "10.20.2.0/24"]
+}
+
+variable "azs" {
+	type = "list"
+	default = ["ap-southeast-2", "ap-southeast-2"]
+}
